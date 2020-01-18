fd9a741fe7f29ea2f441bf41b65791a0e78f4484
Stefan Bokic <stefanbokic731@gmail.com>
2020-01-18 18:42:40 +0100

    added log in and log out and navbar

--- miniGithub/views.py
+++ miniGithub/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
+
+from miniGithub.forms import LoginForm
 from .forms import SignUpForm
 from django.urls import reverse
+from django.contrib import messages
+
 
 def home_view(request):
     return render(request, 'miniGithub/home.html')
+
 
 def signup_view(request):
     form = SignUpForm(request.POST)
@@ -29,3 +32,27 @@ def signup_view(request):
     else:
         form = SignUpForm()
     return render(request, 'miniGithub/signup.html', {'form': form})
+
+
+def login_view(request):
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect(reverse('home'))
+        else:
+            messages.info(request, 'Wrong email or password!')
+            form = LoginForm()
+            return render(request, 'miniGithub/login.html', {'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'miniGithub/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('login'))
