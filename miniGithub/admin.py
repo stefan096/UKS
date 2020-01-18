11650cee8068cb02c88856a9da7e124a59a88baa
Stefan Bokic <stefanbokic731@gmail.com>
2020-01-18 15:52:38 +0100

    popravljen model i zakomentarisano ono sto ne radi

--- miniGithub/admin.py
+++ miniGithub/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from miniGithub.models import Project, Problem, Custom_Event, Comment, Change_State, Change_Assignee, \
-    Milestone, Change_Milestone, Change_Comment, Change_Code, Label, User
+# from miniGithub.models import Project, Problem, Custom_Event, Comment, Change_State, Change_Assignee, \
+#     Milestone, Change_Milestone, Change_Comment, Change_Code, Label, User
+
+from miniGithub.models import Project, Custom_User, Custom_Event, Milestone, \
+     Label
 
 admin.site.register(Project)
-admin.site.register(Problem)
-admin.site.register(User)
+# admin.site.register(Problem)
+admin.site.register(Custom_User)
 admin.site.register(Custom_Event)
-admin.site.register(Comment)
-admin.site.register(Change_State)
-admin.site.register(Change_Assignee)
+# admin.site.register(Comment)
+# admin.site.register(Change_State)
+# admin.site.register(Change_Assignee)
 admin.site.register(Milestone)
-admin.site.register(Change_Milestone)
-admin.site.register(Change_Comment)
-admin.site.register(Change_Code)
+# admin.site.register(Change_Milestone)
+# admin.site.register(Change_Comment)
+# admin.site.register(Change_Code)
 admin.site.register(Label)
