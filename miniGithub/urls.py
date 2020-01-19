36410161efd03069d92bd2a0df18832477758bc2
Kristina G <pinksugarpill@gmail.com>
2020-01-19 18:28:26 +0100

    Izmenjeno prikazivanje pojedinog projekta - dodati tabovi

--- miniGithub/urls.py
+++ miniGithub/urls.py
@@ -5,9 +5,9 @@ from miniGithub.views.projects import *
 
 urlpatterns = [
     path('', projects_view, name="projects"),
-    path('projects/<int:project_id>/', project_view, name='project_details'),
+    path('projects/<int:project_id>/<str:tab_name>/', project_view, name='project_details'),
     path('projects/<int:project_id>/problems/<int:problem_id>/', problem_view, name='problem_details'),
-    path('projects/<int:project_id>/problems/', add_problem_view, name='add_problem'),
+    path('projects/<int:project_id>/problems/add_problem/', add_problem_view, name='add_problem'),
     path('login/', login_view, name="login"),
     path('signup/', signup_view, name="signup"),
     path('logout/', user_logout, name='logout'),
--- miniGithub/views/projects.py
+++ miniGithub/views/projects.py
@@ -15,14 +15,18 @@ def projects_view(request):
     return render(request, 'miniGithub/projects.html', {'projects': projects})
 
 @login_required
-def project_view(request, project_id):
-    project = get_object_or_404(Project, pk=project_id)
+def project_view(request, project_id, tab_name):
+  project = get_object_or_404(Project, pk=project_id)
+  project_problems = []
+  if (tab_name == 'problems'):
     project_problems = Problem.objects.filter(project=project_id)
-    return render(request, 'miniGithub/project_details.html', {'project': project, 'problems': project_problems})
+  print(project_problems)
+  return render(request, 'miniGithub/project_details.html', {'project': project, 'problems': project_problems, 'tab_name': tab_name})
 
 @login_required
 def problem_view(request, project_id, problem_id):
   problem = get_object_or_404(Problem, pk=problem_id)
+  
   return render(request, 'miniGithub/problem_details.html', {'problem': problem})
 
 @login_required
@@ -34,7 +38,7 @@ def add_problem_view(request, project_id):
     title = form.cleaned_data.get('title')
     description = form.cleaned_data.get('description')
     problem = Problem.create(title, description, project, current_user)
-    return redirect(reverse('project_details', kwargs={'project_id':project_id}))
+    return redirect(reverse('project_details', kwargs={'project_id':project_id, 'tab_name': 'problems'}))
   else:
       form = ProblemForm()
   return render(request, 'miniGithub/add_problem.html', {'form': form, 'project': project})
