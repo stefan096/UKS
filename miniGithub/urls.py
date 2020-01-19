c72d31d616b819d7739826c098527f4c5eb1743d
Stefan Bokic <stefanbokic731@gmail.com>
2020-01-19 18:13:35 +0100

    add and edit projects #3

--- miniGithub/urls.py
+++ miniGithub/urls.py
@@ -6,6 +6,8 @@ from miniGithub.views.projects import *
 urlpatterns = [
     path('', projects_view, name="projects"),
     path('projects/<int:project_id>/', project_view, name='project_details'),
+    path('projects/add_edit/<int:project_id>/', project_add_edit, name='project_add_edit'),
+    path('projects/add_edit/<int:project_id>/project_save', project_save, name='project_save'),
     path('projects/<int:project_id>/problems/<int:problem_id>/', problem_view, name='problem_details'),
     path('login/', login_view, name="login"),
     path('signup/', signup_view, name="signup"),
