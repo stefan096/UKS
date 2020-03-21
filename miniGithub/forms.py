8c50ec2bd02f6e777bb1d1488c96ea478482f521
Kristina G <pinksugarpill@gmail.com>
2020-03-21 10:32:09 +0100

    #53 - A lot of UI enhancements - projects display, problems, forms unification for consistent UI, milestones etc.

--- miniGithub/forms.py
+++ miniGithub/forms.py
@@ -7,10 +7,16 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, help_text='First Name')
     last_name = forms.CharField(max_length=100, help_text='Last Name')
     email = forms.EmailField(max_length=150, help_text='Email')
-
+    
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+    
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+    
 
 
 class LoginForm(forms.Form):
